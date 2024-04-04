476870f7a6d5b03cbd25d04afd378a4a2b394bd7
ayebaishmo <ishmo255super@gmail.com>
2024-04-05 01:00:22 +0300

    Added a function that adds cash to the wallet

--- wallet.py
+++ wallet.py
@@ -9,7 +9,10 @@ class Wallet:
             self.initial_amount  = self.initial_amount- amount
             return f"Hello you have withdrawen {amount} and you balance is {self.initial_amount}"
         
-    
+    def addCash(self, add_amount):
+        self.add_amount = add_amount
+        self.initial_amount = self.initial_amount + add_amount
+        return f"Yo added {add_amount} on your account thus your balance is {self.initial_amount}"
         
 
     def __repr__(self):
@@ -22,6 +25,8 @@ if __name__=='__main__':
     print(wallet1.spend_cash(20))
     print(wallet1.spend_cash(120))
     print(wallet1.spend_cash(80))
+    print(wallet1.addCash(200))
+    print(wallet1.spend_cash(50))
 
 
     
