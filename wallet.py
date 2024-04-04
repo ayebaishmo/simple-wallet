ff8b0c356c076debecae7f95cbfcdd632eea172b
ayebaishmo <ishmo255super@gmail.com>
2024-04-05 00:55:18 +0300

    Made transactions

--- wallet.py
+++ wallet.py
@@ -10,6 +10,18 @@ class Wallet:
             return f"Hello you have withdrawen {amount} and you balance is {self.initial_amount}"
         
     
+        
+
+    def __repr__(self):
+        return f"Your balance is: {self.initial_amount}"
+    
+
+if __name__=='__main__':
+    wallet1 = Wallet(100)
+    print(wallet1)
+    print(wallet1.spend_cash(20))
+    print(wallet1.spend_cash(120))
+    print(wallet1.spend_cash(80))
 
 
     
